e71806501810804077632ceaa948ba1343f1ab6e
gempir <daniel.pasch.s@gmail.com>
2020-04-20 21:42:37 +0200

    first steps reading a file and evaluating contents

---
+++ README.md
@@ -0,0 +1,12 @@
+# Run
+
+```
+git submodule update --init --recursive 
+
+
+mkdir build && cd build && cmake .. && make -j8
+```
+
+# TIL
+
+- Cmake makes projects, you don't need to run it all the time, make is enough for recompiling
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,80 @@
 #include <iostream>
 #include <cpr/cpr.h>
 #include "gempir.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string>
+#include <sstream>
+#include <vector>
 
-int main() {
-    std::cout << GEMPIR << std::endl;
+std::vector<std::string> split(const std::string &s, char delim)
+{
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> elems;
+    while (std::getline(ss, item, delim))
+    {
+        elems.push_back(item);
+    }
 
-    auto r = cpr::Get(cpr::Url{"https://gist.githubusercontent.com/fourtf/eb2c086f7639bb9f5a740dc7c2c40753/raw/cada2f7e1c56fc73439be2a7656d03203800d224/.txt"});
-    // r.status_code;
-    // r.headers["content-type"];
+    return elems;
+}
 
-    std::cout << "CURL result: " << r.text;
+
+
+int main()
+{
+    // std::cout << GEMPIR << std::endl;
+
+    std::ifstream file("../log.txt");
+    std::string line;
+
+    std::int64_t lineCount = 0;
+    std::int64_t wordCount = 0;
+    std::int64_t privmsgCount = 0;
+    std::int64_t clearchatCount = 0;
+    std::int64_t usernoticeCount = 0;
+    while (std::getline(file, line))
+    {
+        lineCount++;
+        auto words = split(line, ' ');
+        wordCount += words.size();
+
+        if  (words[2] == "PRIVMSG") {
+            privmsgCount++;
+        } else if (words[2] == "CLEARCHAT")
+        {
+            clearchatCount++;   
+        } else if (words[2] == "USERNOTICE") {
+            usernoticeCount++;
+        } else {
+            std::cout << words[2] + "\n";
+        }
+    }
+
+    std::cout << std::printf("Line count: %d", lineCount);
+    std::cout << "\n";
+    std::cout << std::printf("Word count: %d", wordCount);
+    std::cout << "\n";
+    std::cout << std::printf("Privmsg count: %d", privmsgCount);
+    std::cout << "\n";
+    std::cout << std::printf("Clearchat count: %d", clearchatCount);
+    std::cout << "\n";
+    std::cout << std::printf("Usernotice count: %d", usernoticeCount);
+    std::cout << "\n";
 
     return 0;
 }
+
+
+// for (auto it = words.cbegin(); it != words.cend(); it++)
+//         {
+//             if  (*it == "PRIVMSG") {
+//                 privmsgCount++;
+//             } else
+//             {
+//                 std::cout << *it + "\n";
+//             }
+            
+//         }
