6b1f3eeceedb7ce756ddcb072c3a52288501cd70
gempir <daniel.pasch.s@gmail.com>
2020-04-25 19:35:19 +0200

    download images

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,27 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <string>
-#include <sstream>
 #include <vector>
 #include <regex>
-#include <iostream>
-#include <fstream>
 #include <map>
+#include <filesystem>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <cpr/cpr.h>
+#include <functional>
+
+std::hash<std::string> hasher;
+
+namespace fs = std::filesystem;
 
 std::ofstream linkFile ("../links.txt");
 
 std::map<std::string, bool> linksFound;
+
+std::string lastN(std::string input, int n)
+{
+     return input.substr(input.size() - n);
+}
 
 std::vector<std::string> split(const std::string &s, char delim)
 {
@@ -34,16 +44,26 @@ void printMessage(std::string msg)
     std::cout << msg << std::endl;
 }
 
+void downloadLink(std::string link) {
+    auto r = cpr::Get(cpr::Url{link}, cpr::VerifySsl(false));
+    
+    std::string filename = std::to_string(std::hash<std::string>{}(link));
+    filename += lastN(link, 4);
+
+    std::ofstream image ("../images/" + filename);
+    image << r.text;
+    image.close();
+}
+
 void handleLink(std::string link) {
     std::map<std::string, bool>::iterator it = linksFound.find(link);
 
     if (it != linksFound.end()) {
-        printMessage("Duplicate found, skipping: " + link);
         return;
     }
 
-    printMessage("Saving link: " + link);
     linkFile << link << std::endl;
+    downloadLink(link);
     linksFound.insert(std::pair<std::string, bool>(link, true));
 }
 
@@ -51,11 +71,16 @@ void handleLine(std::string line)
 {
     try
     {
-        std::regex re("(https://[^ ]*)");
+        std::regex re("(https://[^ ]*(\\.jpg|\\.png|\\.gif)|https://gyazo.com/[^ ]*)");
         std::smatch match;
         if (std::regex_search(line, match, re) && match.size() > 1)
         {
-            handleLink(match.str(1));
+            auto link = match.str(1);
+            if (link.find("gyazo.com") != std::string::npos) {
+                link += ".png";
+            }
+
+            handleLink(link);
         }
     }
     catch (std::regex_error &e)
@@ -65,21 +90,34 @@ void handleLine(std::string line)
     }
 }
 
+void scanLog(std::string filePath) {
+    printMessage("Opening: " + filePath);
+    std::ifstream file(filePath, std::ios_base::in | std::ios_base::binary);
+    try {
+        boost::iostreams::filtering_istream in;
+        in.push(boost::iostreams::gzip_decompressor());
+        in.push(file);
+        for(std::string str; std::getline(in, str); )
+        {
+            handleLine(str);
+        }
+    }
+    catch(const boost::iostreams::gzip_error& e) {
+         std::cout << e.what() << '\n';
+    }
+}
+
 int main()
 {
-    std::ifstream file("../log.txt");
-    std::string line;
+    std::string path = "../logs";
 
-    std::int64_t lineCount = 0;
-    while (std::getline(file, line))
+    for(auto itEntry = fs::recursive_directory_iterator(path); itEntry != fs::recursive_directory_iterator(); ++itEntry )
     {
-        lineCount++;
-        handleLine(line);
+        scanLog(itEntry->path().string());
     }
-
-    printMessage("Line count: " + std::to_string(lineCount));
 
     linkFile.close();
 
     return 0;
 }
+
