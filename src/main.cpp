78fa8aa48168220c176b78bcad19b0cad18453f0
gempir <daniel.pasch.s@gmail.com>
2020-04-25 10:38:14 +0200

    more functions, first class

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cpr/cpr.h>
-#include "gempir.h"
+#include "twitch.h"
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -21,6 +21,9 @@ std::vector<std::string> split(const std::string &s, char delim)
     return elems;
 }
 
+void printMessage(std::string msg) {
+    std::cout << msg << std::endl;
+}
 
 
 int main()
@@ -41,40 +44,23 @@ int main()
         auto words = split(line, ' ');
         wordCount += words.size();
 
-        if  (words[2] == "PRIVMSG") {
+        if  (words[2] == TwitchMessageTypes::PRIVMSG) {
             privmsgCount++;
-        } else if (words[2] == "CLEARCHAT")
+        } else if (words[2] == TwitchMessageTypes::CLEARCHAT)
         {
             clearchatCount++;   
-        } else if (words[2] == "USERNOTICE") {
+        } else if (words[2] == TwitchMessageTypes::USERNOTICE) {
             usernoticeCount++;
         } else {
             std::cout << words[2] + "\n";
         }
     }
 
-    std::cout << std::printf("Line count: %d", lineCount);
-    std::cout << "\n";
-    std::cout << std::printf("Word count: %d", wordCount);
-    std::cout << "\n";
-    std::cout << std::printf("Privmsg count: %d", privmsgCount);
-    std::cout << "\n";
-    std::cout << std::printf("Clearchat count: %d", clearchatCount);
-    std::cout << "\n";
-    std::cout << std::printf("Usernotice count: %d", usernoticeCount);
-    std::cout << "\n";
+    printMessage("Line count: " + std::to_string(lineCount));
+    printMessage("Word count: " + std::to_string(wordCount));
+    printMessage("Privmsg count: " + std::to_string(privmsgCount));
+    printMessage("Clearchat count: " + std::to_string(clearchatCount));
+    printMessage("Usernotice count: " + std::to_string(usernoticeCount));
 
     return 0;
 }
-
-
-// for (auto it = words.cbegin(); it != words.cend(); it++)
-//         {
-//             if  (*it == "PRIVMSG") {
-//                 privmsgCount++;
-//             } else
-//             {
-//                 std::cout << *it + "\n";
-//             }
-            
-//         }
---
+++ src/twitch.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class TwitchMessageTypes {
+    public:
+        static constexpr const char* PRIVMSG = "PRIVMSG";
+        static constexpr const char* CLEARCHAT = "CLEARCHAT";
+        static constexpr const char* USERNOTICE = "USERNOTICE";
+};
